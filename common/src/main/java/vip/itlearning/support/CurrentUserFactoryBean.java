3d7a7cf484a30e5de811b1462acd518e64063d62
yawpei <yawpei@126.com>
2018-02-07 13:35:51 +0800

    移除common项目

--- common/src/main/java/vip/itlearning/support/CurrentUserFactoryBean.java
+++
@@ -1,9 +0,0 @@
-package vip.itlearning.support;
-
-/**
- * @author yaw
- * @date 2018/1/23 16:41
- */
-public interface CurrentUserFactoryBean {
-    Object getCurrentUser();
-}
--- common/src/test/java/vip/itlearning/system/RoleTest.java
+++
@@ -1,8 +0,0 @@
-package vip.itlearning.system;
-
-/**
- * @author yaw
- * @date 2018/1/23 13:53
- */
-public class RoleTest {
-}
